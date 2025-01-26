bfe16501d61a64e982cde9317f52dacf1ca14fa0
Allwell Ejike <allwellejike16@gmail.com>
2025-01-26 16:20:03 +0100

    Added  functionality to keep menu running

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,12 +1,24 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Journal theJournal = new Journal();
-        Entry anEntry = new Entry();
-        anEntry.Display();
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        while (true)
+        {
+            Console.WriteLine("\nJournal Menu");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display all entries");
+            Console.WriteLine("3. Save journal to a file");
+            Console.WriteLine("4. Load journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Select an option: ");
+
+            string choice = Console.ReadLine();
+        }
 
     }
 }
