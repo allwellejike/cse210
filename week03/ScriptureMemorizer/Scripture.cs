dbe5fc306df4f96d5eebe721a92d010e0bc18851
Allwell Ejike <allwellejike16@gmail.com>
2025-02-04 17:15:37 +0100

    Added class for scripture

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
