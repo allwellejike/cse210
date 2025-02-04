1d877147437fb6aa884121e1f4ecca82752835d2
Allwell Ejike <allwellejike16@gmail.com>
2025-02-04 16:26:42 +0100

    Write public class for reference

---
+++ week03/ScriptureMemorizer/References.cs
@@ -0,0 +1,6 @@
+public class Reference
+{
+    public string Book { get; private set; }
+    public int Chapter { get; private set; }
+    public int StartVerse { get; private set; }
+    public int? EndVerse { get; private set; }
