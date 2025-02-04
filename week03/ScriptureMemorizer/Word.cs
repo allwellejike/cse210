a64657206405702637289b3845a80d7a4c7ea7dd
Allwell Ejike <allwellejike16@gmail.com>
2025-02-04 17:11:46 +0100

    Added "Word" Class

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,10 @@
+public class Word
+{
+    public string Text { get; private set; }
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
+    }
