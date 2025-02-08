4a26b9083e8c62cc89053bf6de2dab1fc45832c2
Allwell Ejike <allwellejike16@gmail.com>
2025-02-08 16:19:18 +0100

    Create a video class with title, author, length and comment

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        _comments = new List<Comment>();
+    }
