cdfe48e1ac06f5e260c7c579ef80d7b1faccef99
Allwell Ejike <allwellejike16@gmail.com>
2025-02-08 20:44:43 +0100

    create customer class

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,11 @@
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    // Commit: Add constructor for Customer class
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
