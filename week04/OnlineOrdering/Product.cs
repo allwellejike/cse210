2966f0494c2fa4b17601f7fc64d15e518872e34f
Allwell Ejike <allwellejike16@gmail.com>
2025-02-08 20:34:38 +0100

    Create product class

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    private string _name;
+    private string _id;
+    private decimal _price;
+    private int _quantity;
