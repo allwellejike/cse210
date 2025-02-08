5f17187c3b46f8c0822b52a31985ae577928998c
Allwell Ejike <allwellejike16@gmail.com>
2025-02-08 20:35:41 +0100

    Add constructor for product class

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -7,3 +7,11 @@ public class Product
     private string _id;
     private decimal _price;
     private int _quantity;
+
+      public Product(string name, string id, decimal price, int quantity)
+    {
+        _name = name;
+        _id = id;
+        _price = price;
+        _quantity = quantity;
+    }
