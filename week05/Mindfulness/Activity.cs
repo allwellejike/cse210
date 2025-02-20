f42c7da520dae3bb460a03062a8492c709987d74
Allwell Ejike <allwellejike16@gmail.com>
2025-02-20 02:26:26 +0100

    Added functionality to start the activity

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -15,3 +15,15 @@ abstract class Activity
         _name = name;
         _description = description;
     }
+
+     public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine($"Starting {_name}\n{_description}");
+        Console.Write("Enter duration in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
+        Run();
+        End();
+    }
