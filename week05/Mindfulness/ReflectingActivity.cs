4b28aa9ea658838e936fc00d56a27788ee4e4194
Allwell Ejike <allwellejike16@gmail.com>
2025-02-20 02:49:21 +0100

    Added functionality

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -4,3 +4,20 @@ class ReflectingActivity : Activity
     private List<string> _questions = new() { "Why was this experience meaningful?", "How did you feel when it was complete?", "What did you learn about yourself?" };
 
     public ReflectingActivity() : base("Reflection Activity", "This activity helps you reflect on moments of strength.") { }
+
+      protected override void Run()
+    {
+        Random rnd = new();
+        string prompt = _prompts[rnd.Next(_prompts.Count)];
+        Console.WriteLine(prompt);
+        ShowSpinner(3);
+        int elapsed = 0;
+        while (elapsed < _duration)
+        {
+            Console.WriteLine(_questions[rnd.Next(_questions.Count)]);
+            ShowSpinner(3);
+            elapsed += 3;
+        }
+    }
+}
+
