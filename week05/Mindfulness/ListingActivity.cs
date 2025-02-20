2e22df8a150d3765e95433300373a184797f0696
Allwell Ejike <allwellejike16@gmail.com>
2025-02-20 02:52:39 +0100

    Added functionality

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -3,3 +3,21 @@ class ListingActivity : Activity
     private List<string> _prompts = new() { "List people you appreciate.", "List your personal strengths.", "List things that make you happy." };
     
     public ListingActivity() : base("Listing Activity", "This activity helps you list positive things in your life.") { }
+
+      protected override void Run()
+    {
+        Random rnd = new();
+        string prompt = _prompts[rnd.Next(_prompts.Count)];
+        Console.WriteLine(prompt);
+        ShowSpinner(3);
+        int count = 0;
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("Enter an item: ");
+            Console.ReadLine();
+            count++;
+        }
+        Console.WriteLine($"You listed {count} items.");
+    }
+}
