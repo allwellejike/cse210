2d3341732cbcfcac2ec505ea0abb38c43e036174
Allwell Ejike <allwellejike16@gmail.com>
2025-02-20 02:45:01 +0100

    Created class for breathing activity

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,17 @@
+class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity helps you relax by guiding your breathing.") { }
+
+    protected override void Run()
+    {
+        int elapsed = 0;
+        while (elapsed < _duration)
+        {
+            Console.WriteLine("Breathe in...");
+            Countdown(4);
+            Console.WriteLine("Breathe out...");
+            Countdown(4);
+            elapsed += 8;
+        }
+    }
+}
