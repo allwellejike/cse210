8bcf521a3f9607f269d7d49ba26b0af67e59d03a
Allwell Ejike <allwellejike16@gmail.com>
2025-02-20 02:19:54 +0100

    Make Base class represent an activity

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+abstract class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
