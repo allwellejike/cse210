615379381185b68a2297695791f0f012eb98b4c2
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 04:13:50 +0100

    Added running class

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,10 @@
+class Running : Activity
+{
+    private double _distance;
+    
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    
