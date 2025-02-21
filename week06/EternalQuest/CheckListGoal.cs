f6e5ed373bf124d37ce11e5c3950354295616888
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 03:57:55 +0100

    Added a constructor

--- week06/EternalQuest/CheckListGoal.cs
+++ week06/EternalQuest/CheckListGoal.cs
@@ -3,3 +3,11 @@ class ChecklistGoal : Goal
     private int _timesCompleted;
     private int _targetCount;
     private int _bonusPoints;
+
+    public ChecklistGoal(string name, string description, int points, int targetCount, int bonusPoints)
+        : base(name, description, points)
+    {
+        _timesCompleted = 0;
+        _targetCount = targetCount;
+        _bonusPoints = bonusPoints;
+    }
