be3249feaa122e785135f0b611022c7280fe1a4e
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 03:51:25 +0100

    Added constructor

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,14 @@
+using System;
+
+abstract class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
