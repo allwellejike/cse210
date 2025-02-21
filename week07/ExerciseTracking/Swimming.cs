08ef4108d28d3397d6e869971a1ad42434c338e8
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 04:12:57 +0100

    Added methods to be implemented in class

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -15,3 +15,13 @@ abstract class Activity
 
     public int GetMinutes() => _minutes;
     public DateTime GetDate() => _date;
+
+     public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min/km";
+    }
+}
