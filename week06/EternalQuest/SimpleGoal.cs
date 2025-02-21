5d593bdd0c9b91e39dc3678816a03610d6fd5c1c
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 03:53:58 +0100

    Added method

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -6,3 +6,21 @@ class SimpleGoal : Goal
     {
         _isComplete = false;
     }
+
+     public override int RecordEvent()
+    {
+        if (!_isComplete)
+        {
+            _isComplete = true;
+            return _points;
+        }
+        return 0;
+    }
+    
+    public override string GetDetailsString()
+    {
+        return ($"[{(_isComplete ? "X" : " ")}] {_name} - {_description}");
+    }
+    
+    public override bool IsComplete() { return _isComplete; }
+}
