6910fe1d584cfbff25cd8c17f520275a3a833843
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 04:18:02 +0100

    Added program class to list activities

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(DateTime.Now, 30, 4.8),
+            new Cycling(DateTime.Now, 45, 20.0),
+            new Swimming(DateTime.Now, 20, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
