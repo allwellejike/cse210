99b12de852f50cf22e00588f67bd9f5101644acd
Allwell Ejike <allwellejike16@gmail.com>
2025-02-21 03:54:43 +0100

    Added Eternalgoal class

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,8 @@
+class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points) {}
+    
+    public override int RecordEvent()
+    {
+        return _points;
+    }
